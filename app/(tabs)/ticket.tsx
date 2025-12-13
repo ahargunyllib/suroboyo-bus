f733851a14071aa3eecc318ed0752ac4540edb92
ahargunyllib <billy.bpm03@gmail.com>
2025-12-13 17:30:28 +0700

    feat: add owned tickets data and implement ticket display in the ticket screen

--- app/(tabs)/ticket.tsx
+++ app/(tabs)/ticket.tsx
@@ -1,12 +1,125 @@
+import { Button } from "@/components/ui/button";
+import { Icon } from "@/components/ui/icon";
 import { Text } from "@/components/ui/text";
-import { View } from "react-native";
+import { ownedTickets } from "@/data/tickets";
+import { router } from "expo-router";
+import { ArrowLeftIcon } from "lucide-react-native";
+import { FlatList, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Badge } from "../../components/ui/badge";
 
-export default function Ticket() {
+export default function Screen() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center">
-      <View>
-        <Text>Ticket</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-row items-center gap-2 bg-white px-4 py-2">
+        <Button onPress={() => router.back()} size="icon" variant="ghost">
+          <Icon as={ArrowLeftIcon} className="text-black" size={24} />
+        </Button>
+        <Text className="font-bold text-black">Tiket</Text>
+      </View>
+      <View className="flex-1 gap-4 px-4">
+        <FlatList
+          className="flex-1"
+          contentContainerClassName="grow gap-4"
+          data={ownedTickets}
+          renderItem={({ item }) => (
+            <View className="relative gap-2 rounded-lg bg-[#D41D07] p-4">
+              <View className="flex-row items-center justify-between gap-2">
+                <Text className="font-bold text-white">
+                  {item.ticketOffer.name}
+                </Text>
+                <Badge className="bg-[#FE554F]">
+                  <Text className="font-semibold text-white text-xs">
+                    {item.status === "used" ? "Sudah Digunakan" : null}
+                    {item.status === "active" ? "Belum Digunakan" : null}
+                    {item.status === "expired" ? "Kadaluarsa" : null}
+                  </Text>
+                </Badge>
+              </View>
+              <View className="my-4 border-white border-t border-dashed" />
+              {item.status === "used" ? (
+                <View>
+                  <View className="flex-row justify-between">
+                    <Text className="font-bold text-white text-xl">
+                      {item.plan.departureTime}
+                    </Text>
+                    <View className="items-center gap-0">
+                      <Text className="font-bold text-white">
+                        {item.plan.durationInMinutes} menit
+                      </Text>
+                      <Text className="text-white text-xs">Perjalanan</Text>
+                    </View>
+                    <Text className="font-bold text-white text-xl">
+                      {item.plan.arrivalTime}
+                    </Text>
+                  </View>
+                  <View className="flex-row items-center justify-between">
+                    <Text className="font-bold text-white">
+                      {item.usedAt.toLocaleDateString("id-ID", {
+                        day: "2-digit",
+                        month: "short",
+                        year: "numeric",
+                      })}
+                    </Text>
+                    <Text className="font-bold text-white">
+                      {item.usedAt.toLocaleDateString("id-ID", {
+                        day: "2-digit",
+                        month: "short",
+                        year: "numeric",
+                      })}
+                    </Text>
+                  </View>
+                </View>
+              ) : null}
+              {item.status === "active" ? (
+                <View>
+                  <Text className="font-semibold text-white">
+                    Berlaku sampai dengan{" "}
+                    <Text className="font-bold text-white">
+                      {item.expiresAt.toLocaleDateString("id-ID", {
+                        day: "2-digit",
+                        month: "long",
+                        year: "numeric",
+                      })}{" "}
+                      pukul{" "}
+                      {item.expiresAt.toLocaleTimeString("id-ID", {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}
+                    </Text>
+                  </Text>
+                </View>
+              ) : null}
+              {item.status === "expired" ? (
+                <View>
+                  <Text className="font-semibold text-white">
+                    Tiket ini kadaluarsa pada{" "}
+                    <Text className="font-bold text-white">
+                      {item.expiresAt.toLocaleDateString("id-ID", {
+                        day: "2-digit",
+                        month: "long",
+                        year: "numeric",
+                      })}{" "}
+                      pukul{" "}
+                      {item.expiresAt.toLocaleTimeString("id-ID", {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}
+                    </Text>
+                  </Text>
+                </View>
+              ) : null}
+              <View
+                className="absolute top-0 left-1/2 size-8 rounded-full bg-white"
+                style={{ transform: [{ translateY: -16 }] }}
+              />
+              <View
+                className="absolute right-1/2 bottom-0 size-8 rounded-full bg-white"
+                style={{ transform: [{ translateY: 16 }] }}
+              />
+            </View>
+          )}
+        />
       </View>
     </SafeAreaView>
   );
--- data/tickets.ts
+++ data/tickets.ts
@@ -14,3 +14,51 @@ export const ticketOffers = [
 export function getTicketOfferById(id: string) {
   return ticketOffers.find((ticket) => ticket.id === id);
 }
+
+export const ownedTickets = [
+  {
+    id: "ticket-001",
+    ticketOfferId: "student-ticket",
+    ticketOffer: {
+      id: "student-ticket",
+      name: "Tiket Pelajar",
+      price: 2500,
+    },
+    status: "active" as const,
+    createdAt: new Date(),
+    expiresAt: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000), // 1 days later
+  },
+  {
+    id: "ticket-002",
+    ticketOfferId: "general-ticket",
+    ticketOffer: {
+      id: "general-ticket",
+      name: "Tiket Umum",
+      price: 5000,
+    },
+    status: "expired" as const,
+    createdAt: new Date(Date.now() - 10 * 24 * 60 * 60 * 1000), // 10 days ago
+    expiresAt: new Date(Date.now() - 5 * 24 * 60 * 60 * 1000), // 5 days ago
+  },
+  {
+    id: "ticket-003",
+    ticketOfferId: "general-ticket",
+    ticketOffer: {
+      id: "general-ticket",
+      name: "Tiket Umum",
+      price: 5000,
+    },
+    status: "used" as const,
+    createdAt: new Date(Date.now() - 3 * 24 * 60 * 60 * 1000), // 3 days ago
+    expiresAt: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000), // 2 days later
+    usedAt: new Date(Date.now() - 1 * 24 * 60 * 60 * 1000), // 1 day ago
+    routeId: 2,
+    planId: 1,
+    plan: {
+      id: 1,
+      departureTime: "19:27",
+      arrivalTime: "19:56",
+      durationInMinutes: 29,
+    },
+  },
+];
