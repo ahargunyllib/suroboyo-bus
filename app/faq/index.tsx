c5c0aa84b90469fc57202208b10544b088678f41
ahargunyllib <billy.bpm03@gmail.com>
2026-02-21 13:00:12 +0700

    feat: enhance FAQ section with dynamic type filtering and improved layout

--- app/faq/index.tsx
+++ app/faq/index.tsx
@@ -3,6 +3,7 @@ import { Icon } from "@/components/ui/icon";
 import { Text } from "@/components/ui/text";
 import { router } from "expo-router";
 import { ArrowLeftIcon } from "lucide-react-native";
+import { useState } from "react";
 import { ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
@@ -12,9 +13,18 @@ import {
   AccordionTrigger,
   useItemContext,
 } from "../../components/ui/accordion";
-import { faqs } from "../../data/faq";
+import {
+  faqTypes,
+  getFaqByType,
+  getFaqTypeById,
+  type faqs,
+  type FAQType,
+} from "../../data/faq";
+import { cn } from "../../lib/utils";
 
 export default function Screen() {
+  const [type, setType] = useState<FAQType>("operational");
+
   return (
     <SafeAreaView className="flex-1 bg-[#F3F5F9]">
       <View className="flex-row items-center gap-2 bg-[#F3F5F9] px-4 py-2">
@@ -24,14 +34,47 @@ export default function Screen() {
         <Text className="font-bold text-black">FAQ</Text>
       </View>
       <ScrollView className="flex-1 px-4" contentContainerClassName="gap-4">
-        <Text className="font-bold text-[#A6A6A6] text-xs">
-          Pertanyaan yang sering diajukan oleh pengguna.
-        </Text>
-        <Accordion className="w-full gap-2" collapsible type="single">
-          {faqs.map((faq) => (
-            <FaqItem faq={faq} key={faq.id} />
+        <View className="items-center justify-center">
+          <Text className="text-center font-bold text-[#4CD964]">
+            Pertanyaan yang sering diajukan
+          </Text>
+          <Text className="text-center font-bold text-[#A6A6A6] text-xs">
+            Temukan jawaban terbaik dari pertanyaan atau permasalahan Anda
+          </Text>
+        </View>
+        <View className="flex-row flex-wrap gap-2">
+          {faqTypes.map((faqType) => (
+            <Button
+              className={cn(
+                "rounded-lg shadow-none",
+                faqType.id === type
+                  ? "bg-[#F70000] text-white active:bg-[#F70000]/80"
+                  : "border border-[#F70000] bg-transparent active:bg-[#F70000]/10"
+              )}
+              key={faqType.id}
+              onPress={() => setType(faqType.id as FAQType)}
+            >
+              <Text
+                className={cn(
+                  "font-bold text-xs",
+                  faqType.id === type ? "text-white" : "text-[#F70000]"
+                )}
+              >
+                {faqType.name}
+              </Text>
+            </Button>
           ))}
-        </Accordion>
+        </View>
+        <View className="gap-2">
+          <Text className="font-semibold text-xs">
+            Pertanyaan Seputar {getFaqTypeById(type)?.name}
+          </Text>
+          <Accordion className="w-full gap-2" collapsible type="single">
+            {getFaqByType(type).map((faq) => (
+              <FaqItem faq={faq} key={faq.id} />
+            ))}
+          </Accordion>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
--- data/faq.ts
+++ data/faq.ts
@@ -1,20 +1,149 @@
+export type FAQ = {
+  id: string;
+  question: string;
+  answer: string | string[];
+  type: "operational" | "schedule" | "payment" | "ticketing" | "luggage";
+};
+
+export type FAQType = FAQ["type"];
+
+export const faqTypes = [
+  { id: "operational", name: "Operasional" },
+  { id: "schedule", name: "Jadwal Keberangkatan" },
+  { id: "payment", name: "Pembayaran" },
+  { id: "ticketing", name: "Tiket" },
+  { id: "luggage", name: "Barang Bawaan" },
+] as const;
+
+export function getFaqTypeById(id: FAQType) {
+  return faqTypes.find((faqType) => faqType.id === id);
+}
+
 export const faqs = [
   {
-    id: 1,
+    id: "operational-001",
+    question: "Operasional Suroboyo Bus Jam Berapa?",
+    answer: "Operasional Suroboyo Bus Jam 05.30 - 21.00",
+    type: "operational" as const,
+  },
+  {
+    id: "operational-002",
+    question: "Halte yang beroperasi tiap koridor?",
+    answer:
+      "Halte yang beroperasi tiap koridor dapat dilihat melalui instagram @Suroboyobus",
+    type: "operational" as const,
+  },
+  {
+    id: "operational-003",
+    question: "Jam keberangkatan akhir pukul berapa?",
+    answer: "Jam Keberangkatan akhir pukul 20.00",
+    type: "operational" as const,
+  },
+  {
+    id: "operational-004",
     question:
       "Apakah bisa naik dan turun bus Suroboyo Bus tidak melalui halte?",
     answer: "Naik dan turun Bus Suroboyo Bus wajib melalui halte",
+    type: "operational" as const,
   },
   {
-    id: 2,
+    id: "schedule-001",
+    question: "Jadwal keberangkatan tiap koridor",
+    answer:
+      "Jadwal keberangkatan dapat ditinjau melalui instagram @Suroboyobus",
+    type: "schedule" as const,
+  },
+  {
+    id: "payment-001",
+    question: "Berapa tarif naik Suroboyo Bus?",
+    answer:
+      "Tarif sekali jalan untuk umum Rp 5.000 dan pelajar/mahasiswa Rp 2.500",
+    type: "payment" as const,
+  },
+  {
+    id: "payment-002",
+    question: "Apakah anak-anak dikenakan tarif?",
+    answer:
+      "Anak-anak mulai usia 4 tahun atau Tinggi Badan 90 cm sudah dikenakan tarif normal Rp 5.000",
+    type: "payment" as const,
+  },
+  {
+    id: "payment-003",
+    question: "Bagaimana sistem pembayaran Suroboyo Bus?",
+    answer: "Pembayaran dapat dilakukan dengan tunai dan non tunai.",
+    type: "payment" as const,
+  },
+  {
+    id: "payment-004",
+    question: "Pembayaran non tunai apa saja yang disediakan?",
+    answer:
+      "Pembayaran non tunai bisa dilakukan melalui pembayaran QRIS pada aplikasi Suroboyo Bus",
+    type: "payment" as const,
+  },
+  {
+    id: "ticketing-001",
     question: "Bagaimana cara membeli tiket Suroboyo Bus?",
     answer:
       "Tiket dapat dibeli melalui aplikasi Suroboyo Bus atau langsung saat naik Suroboyo Bus",
+    type: "ticketing" as const,
   },
   {
-    id: 3,
-    question: "Berapa tarik naik Suroboyo Bus?",
+    id: "ticketing-002",
+    question:
+      "Apakah bisa membeli tiket lebih dari 1 di aplikasi Suroboyo Bus?",
     answer:
-      "Tarif sekali jalan untuk umum Rp 5.000 dan pelajar/mahasiswa Rp 2.500",
+      "Tiket bisa dibeli lebih dari 1 namun pembelian dilakukan satu per satu",
+    type: "ticketing" as const,
+  },
+  {
+    id: "ticketing-003",
+    question:
+      "Apakah tiket bisa digunakan di hari yang berbeda dari waktu pembelian?",
+    answer:
+      "Tiket bus tetap dapat digunakan selama barcode belum terscan oleh petugas Pramugara/i",
+    type: "ticketing" as const,
+  },
+  {
+    id: "ticketing-004",
+    question: "Bagaimana sistem tiket integrasi antar koridor?",
+    answer:
+      "Penumpang cukup melakukan 1x pembayaran dengan waktu maks 2 jam saat pindak koridor tidak dikenakan tambahan biaya",
+    type: "ticketing" as const,
+  },
+  {
+    id: "luggage-001",
+    question: "Apakah diperbolehkan membawa hewan?",
+    answer: "Naik Suroboyo Bus tidak diperkenankan membawa hewan peliharaan",
+    type: "luggage" as const,
+  },
+  {
+    id: "luggage-002",
+    question: "Apakah diperbolehkan membawa makanan?",
+    answer:
+      "Penumpang Suroboyo Bus tidak diperkenankan membawa makanan berbau menyengat seperti Durian, Jengkol, Ikan/Ayam/Daging Mentah",
+    type: "luggage" as const,
+  },
+  {
+    id: "luggage-003",
+    question: "Apakah diperbolehkan membawa sepeda lipat?",
+    answer:
+      'Ketentuan membawa sepeda lipat max size 16" dalam kondisi terlipat dan saat bus sedang tidak ramai',
+    type: "luggage" as const,
+  },
+  {
+    id: "luggage-004",
+    question: "Bagaimana prosedur jika ada barang tertingal?",
+    answers: [
+      "Penumpang menghubungi Whatsapp CS Suroboyo Bus",
+      "Menunjukan tiket naik bus",
+      "Penumpang datang ke Kantor Admin Kasir di Pool terdekat",
+      "Mengisi form laporan barang tertinggal",
+      "Melakukan swafoto sebagai bukti laporan pengambilan",
+    ],
+    type: "luggage" as const,
   },
 ];
+
+export function getFaqByType(type: FAQType) {
+  return faqs.filter((faq) => faq.type === type);
+}
